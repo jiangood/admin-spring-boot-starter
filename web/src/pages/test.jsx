8de9e70a0cd2c68c8e2fbe6f58841ecb7db2f482
jiangtao <jiangtao.cctv@qq.com>
2025-11-20 10:08:22 +0800

    init

--- web/src/pages/test.jsx
+++ web/src/pages/test.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import {Card} from "antd";
-import {formRegistry} from "@/framework";
 
 export default class extends React.Component {
 
 
     render() {
-        let allKeys = formRegistry.getAllKeys();
-        console.log('所有表单：', allKeys)
 
-        const DemoForm = formRegistry.get('demoForm')
         return <Card title='测试页面'>
 
 
-<DemoForm />
 
         </Card>
     }
