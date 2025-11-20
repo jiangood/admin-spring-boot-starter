03ed275481d8cfa6b0bece0dc111f25b2ed051ef
jiangtao <jiangtao.cctv@qq.com>
2025-11-20 09:58:53 +0800

    init

---
+++ web/config/index.ts
@@ -0,0 +1,4 @@
+export   {default as config} from './config';
+export   {default as configLocal} from './config.local';
+export   {default as routePlugin} from './plugins/route-plugin';
+export   {default as formPlugin} from './plugins/form-plugin';
