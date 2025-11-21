44a12f6589e0e2759e9ab35afde5822d5a5bac3e
jiangtao <jiangtao.cctv@qq.com>
2025-11-21 14:13:35 +0800

    init

--- src/main/java/io/admin/modules/api/controller/ApiAccessLogController.java
+++ src/main/java/io/admin/modules/api/controller/ApiAccessLogController.java
@@ -1,18 +1,40 @@
 package io.admin.modules.api.controller;
 
-import io.admin.modules.api.service.ApiAccessLogService;
+import io.admin.common.dto.AjaxResult;
+import io.admin.framework.data.query.JpaQuery;
 import io.admin.modules.api.entity.ApiAccessLog;
-import io.admin.framework.persistence.BaseController;
+import io.admin.modules.api.service.ApiAccessLogService;
 import jakarta.annotation.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("admin/apiAccessLog")
-public class ApiAccessLogController  extends BaseController<ApiAccessLog>{
+public class ApiAccessLogController  {
 
     @Resource
     ApiAccessLogService service;
+
+
+
+    @Deprecated
+    @RequestMapping("page")
+    public AjaxResult page(@PageableDefault(direction = Sort.Direction.DESC, sort = "updateTime") Pageable pageable) throws Exception {
+        JpaQuery<ApiAccessLog> q = new JpaQuery<>();
+
+
+        Page<ApiAccessLog> page = service.pageByRequest(q, pageable);
+
+
+
+        return AjaxResult.ok().data(page);
+    }
+
+
 
 
 
