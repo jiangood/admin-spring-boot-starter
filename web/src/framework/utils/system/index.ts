4d41ec75f926133cdcdcfe518dc83afacc2c779f
jiangtao <jiangtao.cctv@qq.com>
2025-11-25 14:03:10 +0800

    ts

--- web/src/framework/utils/system/index.ts
+++ web/src/framework/utils/system/index.ts
@@ -1,12 +1,7 @@
-export * from './dict'
+export * from './DictUtils'
 export * from './PermUtils'
-
-
 export * from './PageUtils'
-
-
-export  * from "./SysUtils"
-
+export * from "./SysUtils"
 export * from './HttpUtils'
-export * from './formRegistry'
+export * from './FormRegistryUtils'
 export * from './ThemeUtils'
