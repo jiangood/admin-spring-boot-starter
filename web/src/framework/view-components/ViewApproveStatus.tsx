d7b543b52415ad991f7db595d436a347ddde821b
jiangood <410518072@qq.com>
2025-12-30 14:29:17 +0800

    增加查看审批状态组件

--- web/src/framework/view-components/ViewApproveStatus.tsx
+++ web/src/framework/view-components/ViewApproveStatus.tsx
@@ -1,7 +1,16 @@
 import {DictUtils} from "../utils";
 import {Tag} from "antd";
 
-export function ViewApproveStatus(props) {
+interface ViewProps {
+    value: String;
+}
+
+/**
+ * 查看审批状态组件
+ * @param props
+ * @constructor
+ */
+export function ViewApproveStatus(props:ViewProps) {
     let {value} = props;
     let txt=   DictUtils.dictLabel('approveStatus', value)
 
