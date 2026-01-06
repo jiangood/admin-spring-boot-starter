0ea7543f0070b67fb5237a35330b7aa298b6b902
jiangood <410518072@qq.com>
2026-01-06 11:47:19 +0800

    add: 流程元数据dao层

--- src/main/java/io/github/jiangood/sa/modules/flowable/core/config/FlowableDataInit.java
+++ src/main/java/io/github/jiangood/sa/modules/flowable/core/config/FlowableDataInit.java
@@ -3,11 +3,14 @@ package io.github.jiangood.sa.modules.flowable.core.config;
 import io.github.jiangood.sa.framework.config.init.SystemHookEventType;
 import io.github.jiangood.sa.framework.config.init.SystemHookService;
 import io.github.jiangood.sa.modules.flowable.core.config.meta.ProcessMeta;
+import io.github.jiangood.sa.modules.flowable.core.service.ProcessMetaService;
 import io.github.jiangood.sa.modules.flowable.core.service.ProcessService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Slf4j
@@ -16,7 +19,7 @@ import org.springframework.stereotype.Component;
 public class FlowableDataInit implements CommandLineRunner {
 
 
-    private ProcessMetaConfiguration processConfiguration;
+    private ProcessMetaService processMetaService;
 
     private SystemHookService systemHookService;
     private ProcessService processService;
@@ -24,12 +27,10 @@ public class FlowableDataInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (ProcessMeta meta : processConfiguration.getList()) {
-
+        List<ProcessMeta> list = processMetaService.findAll();
+        for (ProcessMeta meta : list) {
             String key = meta.getKey();
-
             processService.createProcessDefinition(meta);
-
 
             log.info("注册流程定义类 {} {}", key, meta.getClass().getName());
             systemHookService.trigger(SystemHookEventType.AFTER_FLOWABLE_DEFINITION_INIT);
