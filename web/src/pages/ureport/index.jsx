c87a98a16704855e82179a28e898ccf41a44265e
jiangood <410518072@qq.com>
2026-01-19 11:45:55 +0800

    refactor: 去掉ureport,独立成单独插件

--- web/src/pages/ureport/index.jsx
+++ web/src/pages/ureport/index.jsx
@@ -3,15 +3,9 @@ import {Button, Flex} from "antd";
 
 export default class extends React.Component {
 
-
-
     render() {
         return <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-
-
             <Button type='primary' href='/ureport/designer'>打开设计器</Button>
-
-
         </div>
     }
 
