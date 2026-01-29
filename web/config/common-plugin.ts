6c8607ed976da76df669108e433929e6d33b0d67
jiangood <410518072@qq.com>
2026-01-29 17:37:41 +0800

    独立出流程页面

--- web/config/common-plugin.ts
+++ web/config/common-plugin.ts
@@ -1,6 +1,6 @@
 import {IApi} from 'umi';
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 
 const pkgName = '@jiangood/admin-spring-boot-starter';
 
