c874dff66d3ba6831073ea040d47f880bfdf9936
jiangood <410518072@qq.com>
2026-02-09 16:28:34 +0800

    引入 api文档管理

--- src/main/java/io/github/jiangood/openadmin/framework/data/BaseEntity.java
+++ src/main/java/io/github/jiangood/openadmin/framework/data/BaseEntity.java
@@ -3,6 +3,7 @@ package io.github.jiangood.openadmin.framework.data;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.github.jiangood.openadmin.framework.data.id.GenerateUuidV7;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
@@ -20,7 +21,7 @@ import java.io.Serializable;
 @EqualsAndHashCode(of = "id", callSuper = false)
 public abstract class BaseEntity extends BaseNoIdEntity implements Serializable {
 
-
+    @Schema(description = "唯一标识", hidden = true)
     @Id
     @GenerateUuidV7
     @Column(length = DBConstants.LEN_ID)
