3c9258de0d3f196447df8090094bc05b243bb930
jiangood <jiangood@outlook.com>
2026-02-13 20:34:58 +0800

    移除basedao

--- src/test/java/io/github/jiangood/openadmin/modules/system/repository/SysRoleRepositoryTest.java
+++ src/test/java/io/github/jiangood/openadmin/modules/system/repository/SysRoleRepositoryTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 public class SysRoleRepositoryTest {
 
     @Autowired
